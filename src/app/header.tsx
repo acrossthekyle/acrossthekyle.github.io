24f54199a192f822ffc95a5e3ce7e9735c5ae7ed
Kyle <kylegilbert87@gmail.com>
2024-03-22 17:11:12 -0500

    wip

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -5,9 +5,15 @@ import styles from 'scss/app/header.module.scss'
 export default function Header() {
   return (
   	<header className={styles.header}>
-  	  <Link className={styles.logo} href="/" title="Take me home, Wadsworth" aria-label="home">
-  	  	<div />
+  	  <Link href="/" aria-label="home">
+        <div />
   	  </Link>
+      <h1>Kyle</h1>
+      <h2>
+        Thru-hiker by design<br />
+        Developer by trade
+      </h2>
+      <h3>Chicago</h3>
   	</header>
   );
 }
