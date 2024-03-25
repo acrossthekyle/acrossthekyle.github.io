bb5ab9362a59fee42151dc9aef10023ddeda3ad2
Kyle <kylegilbert87@gmail.com>
2024-03-25 15:57:25 -0500

    wip

--- src/app/@details/travels/[year]/[id]/page.tsx
+++ src/app/@details/travels/[year]/[id]/page.tsx
@@ -1,7 +1,24 @@
+'use client'
+
+import { useState } from 'react';
+
+import travels from 'data/travels'
+import styles from 'scss/app/travels/travel/page.module.scss'
+
 export default function Page({ params }) {
+  const travel = travels.find(({ id, year }) => id === params.id && String(year) === params.year);
+
   return (
-    <p>
-    	Some Details for {params.year} {params.id}
-    </p>
+    <section className={styles.slot}>
+      <p>
+        Some description
+      </p>
+      <button type="button">Stats</button>
+      <button type="button">Info</button>
+      <button type="button">Share</button>
+      <div className={styles.travel} id="image-label">
+        {travel.title.join(' ')}
+      </div>
+    </section>
   );
 }
--- src/app/travels/[year]/[id]/page.tsx
+++ src/app/travels/[year]/[id]/page.tsx
@@ -1,7 +1,17 @@
+import travels from 'data/travels'
+import styles from 'scss/app/travels/travel/page.module.scss';
+
 export default function Page({ params }) {
+  const travel = travels.find(({ id, year }) => id === params.id && String(year) === params.year);
+
   return (
-    <>
-      Travel: {params.year} / {params.id}
-    </>
+    <article className={styles.travel}>
+      <div
+        aria-labelledby="image-label"
+        key={travel.id}
+        role="img"
+        style={{ backgroundImage: `url(${travel.image})` }}
+      />
+    </article>
   );
 }
