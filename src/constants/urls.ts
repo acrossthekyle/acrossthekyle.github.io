9c36f23a0de714f1625a1320a3fea99dbf3a5ed7
Kyle Gilbert <hello@acrossthekyle.com>
2025-04-10 12:20:54 -0500

    Linter update

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -4,4 +4,5 @@ export const IMAGE_BASE_URL = 'https://ik.imagekit.io/acrossthekyle/uploads/';
 
 export const RESUME_URL = '/kyle-gilbert-resume.pdf';
 
-export const ME_URL = 'e2b569ce-5d9f-48bb-a456-8f904e109c8e.jpeg?updatedAt=1744299019863';
+export const ME_URL =
+  'e2b569ce-5d9f-48bb-a456-8f904e109c8e.jpeg?updatedAt=1744299019863';
