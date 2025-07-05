84f1b3469cb96c86de854e58118da42ae008bde6
Kyle Gilbert <hello@acrossthekyle.com>
2025-07-05 12:11:04 -0500

    Removed unused Link

--- src/views/home/card.tsx
+++ src/views/home/card.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
 import { useCallback, useState } from 'react';
 import { InView } from 'react-intersection-observer';
 
