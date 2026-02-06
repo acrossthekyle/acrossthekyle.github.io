08fcb151ca421724cf798e45ae52545db64ae93b
Kyle Gilbert <hello@acrossthekyle.com>
2026-02-06 16:58:12 -0600

    Added photos

--- src/views/photos/view/constants.ts
+++ src/views/photos/view/constants.ts
@@ -1,5 +1,13 @@
 export const IMAGES = [
   {
+    caption: 'Pyramids, Cairo - Dec 29th, 2022',
+    src: 'art/7ddf27d9-4601-407d-a1dd-39d6d907422c.jpeg',
+  },
+  {
+    caption: 'Highlands, Scotland - May 11th, 2022',
+    src: 'art/e1a364b2-f08d-48e2-a928-3ea6e054a7cd.jpeg',
+  },
+  {
     caption: 'Navy Pier, Chicago - Mar 21st, 2020',
     src: 'art/58148e20-c61b-481b-8ed9-2f9d6ec871a8.jpeg',
   },
@@ -9,6 +17,6 @@ export const IMAGES = [
   },
   {
     caption: 'Aqua Tower, Chicago - Feb 1st, 2019',
-    src: 'art/0255eb47-fe01-4209-b769-8204f368eb21.jpeg',
+    src: 'art/affd35cc-1939-4c5a-a4f1-c34fbc1a8773.jpeg',
   },
 ];
