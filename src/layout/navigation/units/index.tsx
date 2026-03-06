fd45809d7985361af6dad4b384017551048d68cc
Kyle Gilbert <hello@acrossthekyle.com>
2026-03-06 13:44:47 -0600

    Renamed method

--- src/layout/navigation/units/index.tsx
+++ src/layout/navigation/units/index.tsx
@@ -3,7 +3,7 @@
 import { useUnits } from '@/hooks/useUnits';
 import { ButtonBackdrop } from '@/ui/button';
 
-export default function Theme() {
+export default function Units() {
   const { onToggleUnits, units } = useUnits();
 
   return (
