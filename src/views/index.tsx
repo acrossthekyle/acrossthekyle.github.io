6db29f125b03d32f458215e8b90efd4d1c4f1681
Kyle Gilbert <hello@acrossthekyle.com>
2026-03-08 10:01:49 -0500

    Minor content updates

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -8,7 +8,9 @@ export default function View() {
       <span className={styles.slash} role="presentation">/</span>
       <h1>
         <strong>Hi, I'm Kyle</strong>
-        <small>Backpacker and engineer</small>
+        <small>
+          /kī(ə)l/ • Gaelic • Mid 16th century • Channel, sound, or strait
+        </small>
       </h1>
       <p>
         I'm drawn to long trails and wild places, from the Himalayas to Patagonia. Between adventures, I build web apps in Chicago.
