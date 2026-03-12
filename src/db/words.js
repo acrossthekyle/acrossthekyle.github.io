e1036bb6cf7a135f90250097bf55770b3af9844e
Kyle Gilbert <hello@acrossthekyle.com>
2026-03-12 07:55:42 -0500

    Typo fix

--- src/db/words.js
+++ src/db/words.js
@@ -3,7 +3,7 @@ const data = [
     "author": "Kyle Gilbert",
     "slug": "dark-matter-apple-tv",
     "title": "Dark Matter on AppleTV+",
-    "content": "\nAbsolustly loved this show. I read the book several years ago, but didn't remember much from it. As far as I can tell based on breakdowns of the show on YouTube the adaptation followed the book close enough, with some minor deviations here and there.\n\nI hope they don't fall into a writing-trap for season two, and keep it interesting, with some sort of great ending planned.\n",
+    "content": "\nAbsolutely loved this show. I read the book several years ago, but didn't remember much from it. As far as I can tell based on breakdowns of the show on YouTube the adaptation followed the book close enough, with some minor deviations here and there.\n\nI hope they don't fall into a writing-trap for season two, and keep it interesting, with some sort of great ending planned.\n",
     "date": "March 10th, 2026",
     "readingTime": "1",
     "timestamp": 1773118800,
