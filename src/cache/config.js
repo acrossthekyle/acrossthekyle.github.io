c20930fc184ad825893f5fe3fd835e7f7f889a88
Kyle Gilbert <kylegilbert87@gmail.com>
2026-09-24 16:54:30 -0500

    Updated preview image

--- src/cache/config.js
+++ src/cache/config.js
@@ -1,5 +1,5 @@
 const data = {
-  "lastUpdated": "Sep 19th 2026"
+  "lastUpdated": "Sep 24th 2026"
 };
 
 export default data;
--- src/views/index/content/projects.tsx
+++ src/views/index/content/projects.tsx
@@ -23,7 +23,7 @@ export default function Projects() {
       </Heading>
       <Ui.Image
         className={styles.image}
-        src={image('272c5555-4e19-4bac-80f8-843c315a58ff', 'projects', 'png')}
+        src={image('8ce510ae-6cb7-456f-853d-908aa15df594', 'projects', 'png')}
       />
       <Paragraph>
         A personal budgeting application. Check out a read-only demo <External url="https://project-basalt.demos.acrossthekyle.com">here</External>.
